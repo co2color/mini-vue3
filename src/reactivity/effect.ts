44ded44424049fc442ae4eec45d66cae341d6b31
cococolor <819109255@qq.com>
2023-02-08 00:18:59 +0800

    ✨ feat(effect): stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,37 @@
+import { extend } from '../shared'
+
 let currentEffect
 
-class ReactiveEffect {
-  private _fn
+class reactiveEffect {
+  private _fn: any
+  deps = []
+  active = true
+  onStop?: () => void
+  public scheduler: Function | undefined
   // https://ts.xcatliu.com/advanced/class.html#%E5%8F%82%E6%95%B0%E5%B1%9E%E6%80%A7
-  constructor(fn, public scheduler?) {
+  constructor(fn, scheduler?: Function) {
     this._fn = fn
+    this.scheduler = scheduler
   }
   run() {
     currentEffect = this
     return this._fn()
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect(effect: reactiveEffect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
 }
 
 const targetMap = new Map() // 存target
@@ -27,7 +49,12 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+  // 只有使用了effect，才会存在currentEffect
+  if (!currentEffect) {
+    return
+  }
   dep.add(currentEffect)
+  currentEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
@@ -47,7 +74,16 @@ export function trigger(target, key) {
 }
 
 export function effect(fn, options: any = {}) {
-  const eff = new ReactiveEffect(fn, options?.scheduler)
-  eff.run()
-  return eff.run.bind(eff)
+  const eff = new reactiveEffect(fn, options?.scheduler)
+  extend(eff, options)
+  eff.run() // 执行effect的callback(即fn)
+  const runner: any = eff.run.bind(eff)
+  runner.effect = eff
+  return runner
+}
+
+// runner = effect(callback)
+// 因此effect返回的是runner，其中有effect这个属性
+export function stop(runner) {
+  runner.effect.stop()
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -16,3 +16,25 @@ export function reactive(raw) {
     },
   })
 }
+
+// 对于这段代码：
+/* 
+const user = reactive({ age: 19 })
+let double
+effect(() => {
+  console.log(user.age, 'user.age')
+})
+user.age = 20
+*/
+
+// 我的理解：
+// 1.首先执行effect()这个函数
+// 2.eff.run()执行上面effect()括号内的callback，
+//   此时currentEffect就被赋值给了这个callback，
+//   后面set的时候再触发该callback，
+//   相当于又执行了effect()括号内的callback
+//  3.执行callback的时候发现user.age被访问(被proxy劫持，触发get)了，于是执行track()
+//  4.执行track()的时候发现currentEffect存在，
+//    于是dep.add，即把callback添加到dep中，用于后面的trigger执行该callback
+// 5. 执行user.age = 20的时候，触发set，于是执行trigger()
+// 6. 执行trigger()的时候，遍历dep，执行dep中的callback，即effect()括号内的callback，最终实现了类似vue的响应性
