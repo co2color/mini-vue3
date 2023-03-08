f6882fd2609d4d149f903200b8574c8a7db1a3ca
cococolor <819109255@qq.com>
2023-03-08 21:48:35 +0800

    ✨ feat: getCurrentInstance

---
+++ example/currentInstance/App.js
@@ -0,0 +1,15 @@
+import { h, getCurrentInstance } from '../../lib/guide-mini-vue.esm.js'
+import { Foo } from './Foo.js'
+
+export const App = {
+  name: 'App',
+  render() {
+    return h('div', {}, [h('p', {}, 'currentInstance demo'), h(Foo)])
+  },
+  setup() {
+    const instance = getCurrentInstance()
+    setTimeout(() => {
+      console.log(instance, 'Appinstance')
+    }, 1000)
+  },
+}
--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -32,9 +32,11 @@ function setupStatefulComponent(instance) {
   const { setup } = Component
 
   if (setup) {
+    setCurrentInstance(instance)
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     })
+    setCurrentInstance(null)
     handleSetupResult(instance, setupResult)
   }
 }
@@ -48,4 +50,14 @@ function handleSetupResult(instance, setupResult) {
 function finishComponentSetup(instance: any) {
   const Component = instance.type
   instance.render = Component.render
+}
+
+let currentInstance: any = null
+
+export function getCurrentInstance() {
+  return currentInstance
+}
+// set
+export function setCurrentInstance(instance) {
+  currentInstance = instance
 }
