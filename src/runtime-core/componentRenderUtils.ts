199ead10767e7c01b975f0ee8aaaf9a6fc9bf3ae
co2color <819109255@qq.com>
2024-05-20 21:06:52 +0800

    feat: should update comp

--- src/runtime-core/componentRenderUtils.ts
+++ src/runtime-core/componentRenderUtils.ts
@@ -11,4 +11,24 @@ export function shouldUpdateComponent(prevVNode, nextVNode) {
   if (!nextProps) {
     return true;
   }
+  return hasPropsChanged(prevProps, nextProps);
+}
+
+function hasPropsChanged(prevProps, nextProps): boolean {
+  // 依次对比每一个 props.key
+
+  // 提前对比一下 length ，length 不一致肯定是需要更新的
+  const nextKeys = Object.keys(nextProps);
+  if (nextKeys.length !== Object.keys(prevProps).length) {
+    return true;
+  }
+
+  // 只要现在的 prop 和之前的 prop 不一样那么就需要更新
+  for (let i = 0; i < nextKeys.length; i++) {
+    const key = nextKeys[i];
+    if (nextProps[key] !== prevProps[key]) {
+      return true;
+    }
+  }
+  return false;
 }
