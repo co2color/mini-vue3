47c56e83dd2ad92cc5718b318ef47c9be5c192d2
co2color <819109255@qq.com>
2024-05-20 21:06:33 +0800

    feat: should update comp

--- src/runtime-core/componentRenderUtils.ts
+++ src/runtime-core/componentRenderUtils.ts
@@ -1,1 +1,14 @@
-export function shouldUpdateComponent(prevVNode, nextVNode) {}
+export function shouldUpdateComponent(prevVNode, nextVNode) {
+  const { props: prevProps } = prevVNode;
+  const { props: nextProps } = nextVNode;
+  if (prevProps === nextProps) {
+    return false;
+  }
+
+  if (!prevProps) {
+    return !!nextProps;
+  }
+  if (!nextProps) {
+    return true;
+  }
+}
