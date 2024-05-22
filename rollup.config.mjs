be4f5446c6ffaeb5cd347d8d41ca7e00d7b8621d
co2color <819109255@qq.com>
2024-05-22 22:05:51 +0800

    feat

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,18 @@
-import typescript from '@rollup/plugin-typescript'
-import packagejson from './package.json' assert { type: 'json' }
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import packagejson from "./package.json" assert { type: "json" };
 
 export default {
-  input: './src/index.ts',
+  input: "./src/index.ts",
   output: [
     {
       file: packagejson.main,
-      format: 'cjs',
+      format: "cjs",
     },
     {
       file: packagejson.module,
-      format: 'es',
+      format: "es",
     },
   ],
-  plugins: [typescript()],
-}
+  plugins: [typescript(), resolve()],
+};
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,26 +1,27 @@
-export const extend = Object.assign
+export const extend = Object.assign;
 
-export const EMPTY_OBJ = {}
+export const EMPTY_OBJ = {};
 
-export const isObject = val => val !== null && typeof val === 'object'
+export const isObject = (val) => val !== null && typeof val === "object";
 
 export function hasChanged(value, oldValue) {
-  return !Object.is(value, oldValue)
+  return !Object.is(value, oldValue);
 }
 export function hasOwn(val, key) {
-  return Object.prototype.hasOwnProperty.call(val, key)
+  return Object.prototype.hasOwnProperty.call(val, key);
 }
 
 // 驼峰转连字符
 export function hyphenate(str: string) {
-  return str.replace(/\B([A-Z])/g, '-$1').toLowerCase()
+  return str.replace(/\B([A-Z])/g, "-$1").toLowerCase();
 }
 // 连字符转驼峰， add-foo->addFoo
 export function camelize(str: string) {
-  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''))
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ""));
 }
 
 export function capitalize(str: string) {
-  return str.charAt(0).toUpperCase() + str.slice(1)
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
-export const toHandlerKey = (str: string) => (str ? `on${capitalize(str)}` : '')
+export const toHandlerKey = (str: string) =>
+  str ? `on${capitalize(str)}` : "";
