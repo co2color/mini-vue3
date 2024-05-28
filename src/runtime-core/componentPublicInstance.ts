619146f592f1dfe74c4ca6eb65c7abc95e93aa0e
co2color <819109255@qq.com>
2024-05-28 23:09:25 +0800

    feat: inject

--- src/runtime-core/componentPublicInstance.ts
+++ src/runtime-core/componentPublicInstance.ts
@@ -8,19 +8,17 @@ const publicPropertiesMap = {
 export const PublicInstanceProxyHandlers = {
   get({ _: instance }, key) {
     const { setupState, props } = instance
-    if (key in setupState) {
+    if (key in setupState)
       return setupState[key]
-    }
-    if (hasOwn(setupState, key)) {
+
+    if (hasOwn(setupState, key))
       return setupState[key]
-    }
-    else if (hasOwn(props, key)) {
+
+    else if (hasOwn(props, key))
       return props[key]
-    }
 
     const publicGetter = publicPropertiesMap[key]
-    if (publicGetter) {
+    if (publicGetter)
       return publicGetter(instance)
-    }
   },
 }
