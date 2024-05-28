7860a8fba2147d79bde886e6862ea99057ebc425
co2color <819109255@qq.com>
2024-05-28 23:09:35 +0800

    feat: inject

--- src/runtime-core/componentSlots.ts
+++ src/runtime-core/componentSlots.ts
@@ -2,9 +2,8 @@ import { ShapeFlags } from '../shared/ShapeFlags'
 
 export function initSlots(instance, children) {
   const { vnode } = instance
-  if (vnode.shapeFlag & ShapeFlags.SLOT_CHILDREN) {
+  if (vnode.shapeFlag & ShapeFlags.SLOT_CHILDREN)
     normalizeObjectSlots(children, instance.slots)
-  }
 }
 
 function normalizeSlotValue(value) {
